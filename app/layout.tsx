0cb41003f216a385dbb3a09e214bf372d60f0ba8
Gursimranb127 <gursimranb127@gmail.com>
2025-01-06 17:43:21 -0500

    imported the correct file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import '@/app/ui/global.css'
-import { inter } from './ui/fonts';
-export {inter} from '@/app/ui/fonts'
-
+import {inter} from '@/app/ui/fonts';
 export default function RootLayout({
   children,
 }: {
